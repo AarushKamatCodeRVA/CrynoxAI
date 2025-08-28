f7530c462328f247c5d779fc670db26284f48f7e
Aarush Kamat <aarush.kamat@students.coderva.org>
2025-08-28 18:01:05 +0000

    Add initial project files

--- crynoxai/app/tutor/page.tsx
+++ crynoxai/app/tutor/page.tsx
@@ -1,13 +1,33 @@
-import React from "react";
-import { NavbarDemo } from "@/components/NavbarDemo";
+"use client";
 
-export default function TutorPage() {
+import { NavbarDemo } from "@/components/NavbarDemo";
+import { useUser } from "@clerk/clerk-react";
+
+export default function IfSignedIn() {
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return (
+      <>
+        <NavbarDemo />
+        <div>Loading...</div>
+      </>
+    );
+  }
+
+  if (!isSignedIn) {
+    return (
+      <>
+        <NavbarDemo />
+        <div>Sign in to view this page</div>
+      </>
+    );
+  }
+
   return (
-    <div className="h-screen w-screen relative bg-black text-white overflow-hidden">
+    <>
       <NavbarDemo />
-      <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
-        
-      </div>
-    </div>
+      <div>Hello {user.firstName}!</div>
+    </>
   );
 }
